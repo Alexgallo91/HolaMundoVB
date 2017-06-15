3bc794b6b71464052c8f8224fc5b36e5d41dcf97
Abraham Araujo Moreno <alex_91y@hotmail.com>
2017-06-15 01:45:02 -0500

    Add files via upload

---
+++ HolaMundo_VB/HolaMundo_VB/HelloUser.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LabelName
+        '
+        Me.LabelName.AutoSize = True
+        Me.LabelName.Location = New System.Drawing.Point(12, 49)
+        Me.LabelName.Name = "LabelName"
+        Me.LabelName.Size = New System.Drawing.Size(92, 13)
+        Me.LabelName.TabIndex = 0
+        Me.LabelName.Text = "Ingresa tu nombre"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(15, 26)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(208, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(29, 115)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "&OK"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(130, 115)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'HelloUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(253, 182)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.LabelName)
+        Me.Name = "HelloUser"
+        Me.Text = "Hola Mundo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ HolaMundo_VB/HolaMundo_VB/HelloUser.vb
@@ -0,0 +1,33 @@
+﻿Public Class HelloUser
+    Private Sub HelloUser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LabelName.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'cierra el programa
+        MessageBox.Show("Gracias, " & txtName.Text & ", nos vemos!!!", "Despedida")
+        Me.Close()
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+
+    End Sub
+
+    Private Sub txtName_Resize(sender As Object, e As EventArgs) Handles txtName.Resize
+
+    End Sub
+
+    Private Sub LabelName_RightToLeftChanged(sender As Object, e As EventArgs) Handles LabelName.RightToLeftChanged
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        'Display a message box greeting the user
+        MessageBox.Show("Hello, " & txtName.Text &
+        "! Welcome to Visual Basic 2017.", "Hello User Message")
+    End Sub
+End Class
